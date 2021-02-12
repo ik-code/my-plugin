b5c0ecd0dfffd5d516b2ae4cc24274eb629d834e
Ihor Khaletksyi <igo.metacon@gmail.com>
2021-02-12 10:49:35 +0200

    WP My Plugin. OOP master. Displays a message in footer

---
+++ includes/class-my-plugin-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://wp.medi-com.info
+ * @since      1.0.0
+ *
+ * @package    My_Plugin
+ * @subpackage My_Plugin/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    My_Plugin
+ * @subpackage My_Plugin/includes
+ * @author     Ihor Khaletskyi <ihor.khaletskyi@gmail.com>
+ */
+class My_Plugin_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
